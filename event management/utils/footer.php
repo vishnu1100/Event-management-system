c2c93442991c644c3d23addf6aea44de4d2b0cd1
DEV_VISHNU <127679210+vishnu1100@users.noreply.github.com>
2023-05-25 20:20:16 +0530

    Update footer.php

--- event management/utils/footer.php
+++ event management/utils/footer.php
@@ -1,6 +1,6 @@
 <hr class = "footerline"><!--css modified horizontal line-->
 <footer>
     <div class = "container">
-        <h6 style="text-align:center;"> Copyright @ 2022 By: Suman Mallikarjun Hugar</h6>
+        <h6 style="text-align:center;">  Copyright @ 2023 By: <a href="https://github.com/vishnu1100"> VISH_NU </a></h6>
     </div>
 </footer>
